52ff500faba544f7f78f1b66ac01561f767041e9
Aonkyanu C. Tomal <ansatsuken1@outlook.com>
2024-10-07 23:15:38 +0600

    Add color change functionality with mouse hover

--- script.js
+++ script.js
@@ -1,39 +1,48 @@
 const container = document.querySelector(".container");
+const button = document.querySelector("#btn");
 
 function makeRow(rowNum) {
+
     for (let i = 1; i <= rowNum; i++) {
         let row = document.createElement("div");
-        row.className = "rows"
+        row.classList.add("rows");
         container.appendChild(row);
     }
 }
 
 function makeCol(colNum) {
     let rows = document.querySelectorAll(".rows");
+    let colWidth = 600 / colNum;
 
     for (let i = 0; i < rows.length; i++) {
         for (let j = 1; j <= colNum; j++) {
             let cols = document.createElement("div");
-            cols.className = "cols";
+            cols.classList.add("cols");
+            cols.style.width = `${colWidth}px`;
+            cols.style.height = `${colWidth}px`;
             rows[i].appendChild(cols);
+            cols.addEventListener("mouseover", sketch);
         }
     }
 }
 
-function makeRowCol(row, col) {
-    makeRow(row);
-    makeCol(col);
+function sketch(e) {
+    e.target.style.backgroundColor = "#e1f531";
 }
 
-makeRowCol(16, 16);
+function makeRowCol(cellNum) {
+    makeRow(cellNum);
+    makeCol(cellNum);
+}
 
-/* const cols = document.querySelector(".cols");
-cols.addEventListener("mouseover", () => {
-    cols.style.backgroundColor = "#e1f531";
-});
+window.onload = () => {makeRowCol(99)};
 
-function sketch(e) {
-    for (let i = 0; i <= e; i++) {
+button.addEventListener("click", () => {
+    let userInput = prompt("How many squares do you want the grid to have?");
 
+    if (userInput < 0 || userInput > 100) {
+        prompt("Please choose a number between 1-100!");
+    } else {
+        makeRowCol(userInput);
     }
-} */
+});
